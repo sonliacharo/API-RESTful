a50aafcf79e053ea2660464e48d1ea713afa5cf9
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-10-01 17:40:15 -0300

    refactor: refactor categories controller

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,13 +1,13 @@
-const { handleServerError } = require('../utils/server')
-const pool = require('../database/pdv')
+import handleServerError from "../utils/serverError.js";
+import pool from "../database/pdv.js";
 
 const getCategories = async (req, res) => {
-    try {
-        const categories = await pool.query('select * from categorias')
-        return res.status(200).json(categories.rows)
-    } catch (error) {
-        return handleServerError(res)
-    }
-}
+  try {
+    const categories = await pool.query("select * from categorias");
+    return res.status(200).json(categories.rows);
+  } catch (error) {
+    return handleServerError(res);
+  }
+};
 
-module.exports = { getCategories }
+export { getCategories };

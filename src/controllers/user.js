316133d23a0355a2cc39a50ab5e620122ded2a9e
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-10-01 17:32:14 -0300

    refactor: refactor user controller

---
+++ src/controllers/user.js
@@ -0,0 +1,23 @@
+import pool from "../database/pdv.js";
+import encryptPassword from "../utils/encryptPassword.js";
+import handleServerError from "../utils/serverError.js";
+
+const registerUser = async (req, res) => {
+  const { nome, email, senha } = req.body;
+
+  try {
+    const encryptedPassword = await encryptPassword(senha);
+
+    const newUser = await pool.query(
+      `insert into usuarios (nome, email, senha) 
+        values ($1, $2, $3) returning id, nome, email`,
+      [nome, email, encryptedPassword]
+    );
+
+    return res.status(201).json(newUser.rows[0]);
+  } catch (error) {
+    return handleServerError(res);
+  }
+};
+
+export { registerUser };

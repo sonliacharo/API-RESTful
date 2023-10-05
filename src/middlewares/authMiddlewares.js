04a39d2e385182f3fda12edcbc734635c2981440
EltonCampos07 <eltonapcnunes@gmail.com>
2023-10-04 23:39:33 -0300

    feat: criação da rota deletar produto

--- src/middlewares/authMiddlewares.js
+++ src/middlewares/authMiddlewares.js
@@ -1,16 +1,32 @@
-import { findUserByEmail } from '../repositories/userRepository.js'
-import { checkEncrypt } from '../utils/encryptPassword.js';
+import { findUserByEmail, findUserByID } from '../repositories/userRepository.js'
+import { checkEncrypt, validateAndGetTokenData } from '../utils/encryptPassword.js'
 
-const validateLoginData = async (req, res, next) => {
+export const validateToken = async (req, res, next) => {
+  const { authorization } = req.headers
+  const token = authorization?.split(' ')[1]
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token não informado ou inválido.' })
+  }
+
+  try {
+    const { id } = validateAndGetTokenData(token)
+    req.user = await findUserByID(id)
+  } catch (err) {
+    return res.status(401).json({ message: 'Acesso não autorizado.' })
+  }
+
+  next()
+}
+
+export const validateLoginData = async (req, res, next) => {
   const { email, senha } = req.body
   const userData = await findUserByEmail(email)
   const isValidEncrypt = await checkEncrypt(userData?.senha, senha)
 
   if (!isValidEncrypt) {
-    return res.status(401).json('Acesso não autorizado.')
+    return res.status(401).json({ message: 'Acesso não autorizado.' })
   }
 
   next()
-};
-
-export default validateLoginData
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,31 +1,37 @@
-import { Router } from "express";
-import { registerUser, getProfile } from "../controllers/user.js";
-import { getCategories } from "../controllers/categories.js";
-import editProfile from "../controllers/editProfile.js";
-import validateRegistration from "../middlewares/validateRegistration.js";
-import validateLoginData from "../middlewares/authMiddlewares.js";
-import {
-  validateDataRequest,
-  validateRequiredProperties,
-} from "../middlewares/userValidators.js";
-import login from "../controllers/loginController.js";
+import { Router } from 'express'
+import { registerUser, getProfile } from '../controllers/user.js'
+import { getCategories } from '../controllers/categories.js'
+import editProfile from '../controllers/editProfile.js'
+import validateRegistration from '../middlewares/validateRegistration.js'
+import { validateLoginData, validateToken } from '../middlewares/authMiddlewares.js'
+import { validateDataRequest, validateRequiredProperties } from '../middlewares/userValidators.js'
+import login from '../controllers/loginController.js'
 
-import validateEditProfile from "../middlewares/validateEditProfile.js";
+import validateEditProfile from '../middlewares/validateEditProfile.js'
+import { checkIdProduct } from '../middlewares/product/productMiddlewares.js'
+import { deleteProduct } from '../controllers/product/productControlers.js'
 
-export const router = Router();
+export const router = Router()
 
-const requiredAccountProperties = ["email", "senha"];
+const requiredAccountProperties = ['email', 'senha']
 
-router.get("/categoria", getCategories);
-router.post("/usuario", validateRegistration, registerUser);
+router.get('/categoria', getCategories)
+router.post('/usuario', validateRegistration, registerUser)
 
 router.post(
-  "/login",
+  '/login',
   validateRequiredProperties(requiredAccountProperties),
   validateDataRequest,
   validateLoginData,
   login
 );
 
-router.get("/usuario", getProfile);
-router.put("/usuario", validateEditProfile, editProfile);
+//Rotas protegidas
+router.use(validateToken)
+
+//usuário
+router.get("/usuario", getProfile)
+router.put("/usuario", validateEditProfile, editProfile)
+
+//produtos
+router.delete("/produto/:id", checkIdProduct, deleteProduct)

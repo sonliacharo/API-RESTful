b4aa3a0711ddd2f52c884dcaa367f06daadbfde6
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-10-06 21:57:15 -0300

    fix: correção nos values do host e do user na conexão com o banco de dados

--- src/database/pdv.js
+++ src/database/pdv.js
@@ -7,9 +7,9 @@ dotenv.config();
 const { DB_USER, DB_PORT, DB_HOST, DB_PASSWORD, DB_DATABASE } = process.env;
 
 const pool = new Pool({
-  host: DB_USER,
+  host: DB_HOST,
   port: DB_PORT,
-  user: DB_HOST,
+  user: DB_USER,
   password: DB_PASSWORD,
   database: DB_DATABASE,
 });
